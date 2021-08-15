6c5189e8d35b638b4403cf7b58524a8b24a695c7
zhuk-zhenya <88724582+zhuk-zhenya@users.noreply.github.com>
2021-08-15 16:14:57 +0300

    commet

--- 1.py
+++ 1.py
@@ -1,31 +1,9 @@
 from fastapi import FastAPI
-import pydantic
 
 app = FastAPI()
-
-
-class Numbers(pydantic.BaseModel):
-    a: int
-    b: int
-
-
-class TaskArgs(pydantic.BaseModel):
-    name: str
-    args: Numbers
-
-
-@app.put("/task/3/")
-async def handler(obj: TaskArgs):
-    a = obj.args.a
-    b = obj.args.b
-    return {
-        "result": a / b,
-        "task": obj.name,
-    }
-
 
 @app.get("/hello")
 async def handler():
     return {
-        "hello": "world",
+        "hello": "world"
     }
